82093afd498b0bd0166a71bd7e1f9b371f693ad9
Alena Nemtsova <jelenatbs@gmail.com>
2024-06-05 17:40:27 +0300

    ref: function to arrow func, remove extra brackets

--- js/index.js
+++ js/index.js
@@ -3,21 +3,15 @@
 const buttons = document.querySelectorAll('.users__button');
 const cards = document.querySelectorAll('.users-card__content');
 
-const showActive = function (event) {
-    buttons.forEach((button) => (
-        button.classList.remove('active')
-    ))
+const showActive = event => {
+    const type = event.target.dataset.type;
+    const card = document.getElementById(type);
+
+    buttons.forEach(button => button.classList.remove('active'));
     event.target.classList.add('active');
 
-    let type = event.target.dataset.type;
-    let card = document.getElementById(type)
-
-    cards.forEach((card) => (
-        card.classList.remove('active')
-    ))
+    cards.forEach(card => card.classList.remove('active'));
     card.classList.add('active');
 }
 
-buttons.forEach(function (button) {
-    button.addEventListener('click', showActive)
-})
+buttons.forEach(button => button.addEventListener('click', showActive));
